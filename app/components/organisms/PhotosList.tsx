e9985eca09c9a604eacb58648ebf50aa0f33c81b
Carlos Isaias Ruiz <cruiz12@alumnos.uaq.mx>
2023-12-06 12:17:36 -0600

    feat: preparing PhotosList to display content

--- app/components/organisms/PhotosList.tsx
+++ app/components/organisms/PhotosList.tsx
@@ -1,4 +1,8 @@
-import { View, ViewProps, ViewStyle } from "react-native"
+import { Alert, FlatList, View, ViewProps, ViewStyle } from "react-native"
+import { PhotoItem } from "../atoms/PhotoItem"
+import { usePhotoStore } from "../providers/PhotosProvider"
+import { EmptyList } from "../molecules/EmptyList"
+import { PhotoObjType } from "types"
 
 /**
  * This component should be responsible for rendering a
@@ -7,8 +11,55 @@ import { View, ViewProps, ViewStyle } from "react-native"
  * the ability to select photos from those which are currently
  * being displayed.
  */
-export type PhotoListProps = ViewProps & {}
+export type PhotoListProps = ViewProps & {
+  photos: PhotoObjType[]
+  action: "view" | "add" | "remove"
+}
 
-export function PhotosList({ ...props }: PhotoListProps) {
-  return <View {...props}>{/* Do some stuff here to display the photos list. */}</View>
+export function PhotosList({ photos, action, ...props }: PhotoListProps) {
+  const { addUserPhotos, removeUserPhotos } = usePhotoStore()
+
+  const handlePhotoTap = (item: PhotoObjType) => {
+    if (action === "view") {
+      Alert.alert("Not built yet")
+      return
+    }
+
+    if (action === "add") {
+      addUserPhotos([item])
+      return
+    }
+
+    if (action === "remove") {
+      removeUserPhotos([item])
+      return
+    }
+  }
+
+  return (
+    <View {...props}>
+      <FlatList
+        data={photos}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={{ width: 16, backgroundColor: "pink" }} />}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        renderItem={({ item }) => <PhotoItem onPress={() => handlePhotoTap(item)} {...item} />}
+        contentContainerStyle={$containerStyle}
+        columnWrapperStyle={$columnWrapperStyle}
+        ListEmptyComponent={() => <EmptyList notice="You haven't added photos yet" />}
+      />
+    </View>
+  )
+}
+
+const $columnWrapperStyle: ViewStyle = {
+  justifyContent: "space-between",
+  gap: 16,
+}
+
+const $containerStyle: ViewStyle = {
+  paddingTop: 96,
+  paddingBottom: 16,
+  gap: 16,
 }
